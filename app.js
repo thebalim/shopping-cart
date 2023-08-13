a6bd45fbfe103d57982ddfda940d3e96337f5fe4
theba <theba.limbu07@gmail.com>
2023-08-13 09:51:16 +0100

    added cart

--- app.js
+++ app.js
@@ -88,3 +88,14 @@ for (let i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 }
+
+
+
+// show cart function
+const cartInfo = document.getElementById('cart-info');
+const cart = document.getElementById('cart');
+
+cartInfo.addEventListener('click', function() {
+    cart.classList.toggle("show-cart");
+})
+
